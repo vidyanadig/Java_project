0cfdd0827cc7021ac18f7aef9944d5d08b4127a6
Vidya Nadig <mailnadig@gmail.com>
2013-07-27 19:57:35 -0700

    BookMarkers First commit

---
+++ BookMarkersLibrary/src/Resident.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author vidyanadig
+ *
+ */
+public class Resident {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
